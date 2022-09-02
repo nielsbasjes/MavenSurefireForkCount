f2a607c0bc8ef036d222dd2e1c525d6d7f3bec8a
Niels Basjes <niels@basjes.nl>
2022-09-02 17:02:42 +0200

    Initial

---
+++ src/main/java/nl/basjes/bugreport/App.java
@@ -0,0 +1,7 @@
+package nl.basjes.bugreport;
+
+public class App {
+    public String doSomething() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/nl/basjes/bugreport/TestApp.java
@@ -0,0 +1,15 @@
+
+package nl.basjes.bugreport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestApp {
+
+    @Test
+    void testSomething() {
+        assertEquals("Hello", new App().doSomething());
+    }
+
+}
